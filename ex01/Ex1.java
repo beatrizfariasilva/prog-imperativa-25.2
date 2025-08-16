7444b47690209a31b42e18f55029bae1760a4838
beatrizfariasilva <beatrizfariasilva1@gmail.com>
2025-08-16 00:18:52 +0000

    movi para outra pasta

--- ex01/Ex1.java
+++ ex01/Ex1.java
@@ -1,10 +1,11 @@
+package ex01;
 public class Ex1 {
     public static final int QTD_REPETIC=5;
     public static void main (String[] args){
 
         System.out.println("Impressão em ordem descrescente (modo desnecessário)");
         int i=0;
-        for (i=1; i>=QTD_REPETIC; i++){
+        for (i=1; i<=QTD_REPETIC; i++){
             System.out.println(i+ ":" + " Olá, Turma");
         }
         
