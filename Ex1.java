65059e0e2300c7e8a90bbb5d72d7793a7058d0a3
beatrizfariasilva <beatrizfariasilva1@gmail.com>
2025-08-16 00:09:58 +0000

    revisão FOR

---
+++ Ex1.java
@@ -0,0 +1,12 @@
+public class Ex1 {
+    public static final int QTD_REPETIC=5;
+    public static void main (String[] args){
+
+        System.out.println("Impressão em ordem descrescente (modo desnecessário)");
+        int i=0;
+        for (i=1; i>=QTD_REPETIC; i++){
+            System.out.println(i+ ":" + " Olá, Turma");
+        }
+        
+    }
+}
